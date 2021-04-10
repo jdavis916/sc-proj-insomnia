e1a9b2c4c53371724e87a319370134506107885a
Jason Davis <jasondavis3@gmail.com>
2021-04-10 09:39:08 -0700

    default dashboard installed

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET admin dashboard */
 router.get('/', function(req, res, next) {
   res.render('adminDashboard', { 
+  	layout: 'mainAdmin.hbs',
   	title: 'Admin Section',
   	msg: 'This is where your dashboard for the admins would go',
   	pageMainClass: 'pgAdminDashboard'
