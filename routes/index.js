54c2a0ac131adea07c1a7104f54b884797c3c6b1
CjL95 <71044991+CjL95@users.noreply.github.com>
2021-04-10 12:29:40 -0400

    added user schema

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
+//import userModel from "../backend/models/users";
+//const mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
-
+ 
 /* GET home page */
 router.get('/', function(req, res, next) {
   res.render('index', { 
@@ -8,6 +10,56 @@ router.get('/', function(req, res, next) {
   	msg: 'This sample template should help get you on your way.',
   	pageMainClass: 'pgHome'
   });
-});
+})
+.get('/', function(req, res, next) {
+  res.render('index', { 
+  	title: 'Simple Node Template',
+  	msg: 'This sample template should help get you on your way.',
+  	pageMainClass: 'pgHome'
+  });
+})
+.get('/projects', function(req, res, next) {
+  res.render('index', { 
+  	title: 'Simple Node Template',
+  	msg: 'This sample template should help get you on your way.',
+  	pageMainClass: 'pgHome'
+  });
+})
+.get('/featured', function(req, res, next) {
+  res.render('index', { 
+  	title: 'Simple Node Template',
+  	msg: 'This sample template should help get you on your way.',
+  	pageMainClass: 'pgHome'
+  });
+})
+.get('/search', function(req, res, next) {
+  res.render('index', { 
+  	title: 'Simple Node Template',
+  	msg: 'This sample template should help get you on your way.',
+  	pageMainClass: 'pgHome'
+  });
+})
+.get('/contactUs', function(req, res, next) {
+  res.render('index', { 
+  	title: 'Simple Node Template',
+  	msg: 'This sample template should help get you on your way.',
+  	pageMainClass: 'pgHome'
+  });
+})
+.get('/users', function(req, res, next) {
+  res.render('index', { 
+  	title: 'Simple Node Template',
+  	msg: 'This sample template should help get you on your way.',
+  	pageMainClass: 'pgHome'
+  });
+})
+.post('/messages', function(req, res, next) {
+
+})
+
+
+
+
+
 
 module.exports = router;
